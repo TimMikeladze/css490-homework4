5e5d4d15286def65febbad96a1567010fd450d51
Tim Mikeladze <tim.mikeladze@gmail.com>
2014-02-20 13:51:22 -0800

    hide buttons

--- src/com/css490/homework4/fragments/PageFragment.java
+++ src/com/css490/homework4/fragments/PageFragment.java
@@ -16,94 +16,105 @@ import com.css490.homework4.dialogs.ChooseStrokeDialog;
 import com.css490.homework4.view.DrawingView;
 
 public class PageFragment extends Fragment implements OnClickListener, DrawingCallbacks {
-	
-	private static final String DRAWING_OFF = "Drawing off";
-	private static final String DRAWING_ON = "Drawing on";
-	private int imageID;
-	private DrawingView drawingView;
-	private Button drawButton;
-	private Button strokeButton;
-	private Button colorButton;
-	private ChooseColorDialog chooseColorDialog;
-	private ChooseStrokeDialog chooseStrokeDialog;
-	
-	private boolean drawingEnabled;
-	private ToolbarCallbacks toolbarCallbacks;
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		Bundle bundle = getArguments();
-		
-		int layoutID = bundle != null && bundle.containsKey("layoutID") ? bundle.getInt("layoutID") : R.layout.fragment_page_generic;
-		View v = inflater.inflate(layoutID, container, false);
-		
-		drawButton = (Button) v.findViewById(R.id.button_draw);
-		drawButton.setOnClickListener(this);
-		strokeButton = (Button) v.findViewById(R.id.button_stroke);
-		strokeButton.setOnClickListener(this);
-		colorButton = (Button) v.findViewById(R.id.button_color);
-		colorButton.setOnClickListener(this);
-		
-		chooseColorDialog = new ChooseColorDialog(getActivity(), this);
-		chooseStrokeDialog = new ChooseStrokeDialog(getActivity(), this);
-		
-		if (bundle != null) {
-			imageID = bundle.getInt("imageID");
-			drawingView = (DrawingView) v.findViewById(R.id.view_drawing);
-			drawingView.setImage(imageID);
-		}
-		
-		return v;
-	}
-	
-	public void addToolbarCallbacks(ToolbarCallbacks toolbarCallbacks) {
-		this.toolbarCallbacks = toolbarCallbacks;
-	}
-	
-	@Override
-	public void onClick(View v) {
-		if (v.getId() == drawButton.getId()) {
-			toggleDrawing();
-		} else if (v.getId() == strokeButton.getId()) {
-			selectStroke();
-		} else if (v.getId() == colorButton.getId()) {
-			selectColor();
-		}
-	}
-	
-	private void toggleDrawing() {
-		drawingEnabled = !drawingEnabled;
-		drawingView.enableDrawing(drawingEnabled);
-		if (toolbarCallbacks != null) {
-			toolbarCallbacks.toggleScroll(drawingEnabled);
-		}
-		drawButton.setText(drawingEnabled ? DRAWING_ON : DRAWING_OFF);
-		
-	}
-	
-	private void selectStroke() {
-		chooseStrokeDialog.show();
-	}
-	
-	private void selectColor() {
-		chooseColorDialog.show();
-	}
-	
-	@Override
-	public void setColor(int color) {
-		drawingView.setColor(color);
-		setEraser(false);
-	}
-	
-	@Override
-	public void setStroke(float stroke) {
-		drawingView.setStroke(stroke);
-		
-	}
-	
-	@Override
-	public void setEraser(boolean eraser) {
-		drawingView.setEarser(eraser);
-	}
-	
+    
+    private static final String DRAWING_OFF = "Drawing off";
+    private static final String DRAWING_ON = "Drawing on";
+    private int imageID;
+    private DrawingView drawingView;
+    private Button drawButton;
+    private Button strokeButton;
+    private Button colorButton;
+    private ChooseColorDialog chooseColorDialog;
+    private ChooseStrokeDialog chooseStrokeDialog;
+    
+    private boolean drawingEnabled;
+    private ToolbarCallbacks toolbarCallbacks;
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Bundle bundle = getArguments();
+        
+        int layoutID = bundle != null && bundle.containsKey("layoutID") ? bundle.getInt("layoutID")
+                : R.layout.fragment_page_generic;
+        View v = inflater.inflate(layoutID, container, false);
+        
+        drawButton = (Button) v.findViewById(R.id.button_draw);
+        drawButton.setOnClickListener(this);
+        strokeButton = (Button) v.findViewById(R.id.button_stroke);
+        strokeButton.setOnClickListener(this);
+        colorButton = (Button) v.findViewById(R.id.button_color);
+        colorButton.setOnClickListener(this);
+        
+        chooseColorDialog = new ChooseColorDialog(getActivity(), this);
+        chooseStrokeDialog = new ChooseStrokeDialog(getActivity(), this);
+        
+        if (bundle != null) {
+            imageID = bundle.getInt("imageID");
+            drawingView = (DrawingView) v.findViewById(R.id.view_drawing);
+            drawingView.setImage(imageID);
+        }
+        
+        return v;
+    }
+    
+    public void addToolbarCallbacks(ToolbarCallbacks toolbarCallbacks) {
+        this.toolbarCallbacks = toolbarCallbacks;
+    }
+    
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == drawButton.getId()) {
+            toggleDrawing();
+        }
+        else if (v.getId() == strokeButton.getId()) {
+            selectStroke();
+        }
+        else if (v.getId() == colorButton.getId()) {
+            selectColor();
+        }
+    }
+    
+    private void toggleDrawing() {
+        drawingEnabled = !drawingEnabled;
+        drawingView.enableDrawing(drawingEnabled);
+        if (toolbarCallbacks != null) {
+            toolbarCallbacks.toggleScroll(drawingEnabled);
+        }
+        if (drawingEnabled) {
+            strokeButton.setVisibility(View.VISIBLE);
+            colorButton.setVisibility(View.VISIBLE);
+        }
+        else {
+            strokeButton.setVisibility(View.INVISIBLE);
+            colorButton.setVisibility(View.INVISIBLE);
+        }
+        drawButton.setText(drawingEnabled ? DRAWING_ON : DRAWING_OFF);
+        
+    }
+    
+    private void selectStroke() {
+        chooseStrokeDialog.show();
+    }
+    
+    private void selectColor() {
+        chooseColorDialog.show();
+    }
+    
+    @Override
+    public void setColor(int color) {
+        drawingView.setColor(color);
+        setEraser(false);
+    }
+    
+    @Override
+    public void setStroke(float stroke) {
+        drawingView.setStroke(stroke);
+        
+    }
+    
+    @Override
+    public void setEraser(boolean eraser) {
+        drawingView.setEarser(eraser);
+    }
+    
 }
